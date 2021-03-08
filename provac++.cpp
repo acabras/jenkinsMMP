2380c81621e804a81bcc91c19926628b8c96b89b
Alessandro Cabras <alessandro.cabras2@gmail.com>
2021-03-08 14:16:37 +0100

    Update provac++.cpp

--- provac++.cpp
+++ provac++.cpp
@@ -1,2 +1,10 @@
-#include <stdio>
-cout<<"Hello jenkins"<<endl
+#include <iostream>
+using namespace std;
+int main(){
+  cout<<"Hello 1"<<endl;
+  cout<<"Hello 2"<<endl;
+  cout<<"Hello 2"<<endl;
+  cout<<"Hello 3"<<endl;
+    cout<<"Hello 4"<<endl;
+  cout<<"Hello 5"<<endl;
+}
